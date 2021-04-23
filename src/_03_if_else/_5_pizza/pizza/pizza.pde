a31559069838e9350b6acea02a21197a74eddb6a
root <root@3cb25878f3c3>
2021-04-23 00:00:28 +0000

    finished pizza

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,32 @@
 
 void setup() {
+  PImage pepperoni;
+  PImage olive;
+  PImage mushroom;
+ 
   
-    
+  
+  pepperoni = loadImage("pepperoni.png");
+  olive = loadImage("olive.png");
+ mushroom = loadImage("mushroom.png");
+  
+  size(800,800);
+    fill(#F2DF98);
+    ellipse(300,300,300,300);
+    fill(#F53F2F);
+    ellipse(300,300,290,290);
+    fill(#FFEE52);
+    ellipse(300,300,270,270);
+     image(pepperoni,200,200);
+      image(olive,270,200);
+       image(mushroom,330,330);
+        image(pepperoni,267,278);
+        image(pepperoni,239,300);
+        image(pepperoni,200,200);
+        image(olive,270,230);
+        image(mushroom,290,290);
+        image(mushroom,189,234);
+        
 }
 void draw() {
     
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,22 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String firstRiddle= JOptionPane.showInputDialog("What is greater than God,more evil than the devil,the poor have it,the rich need it,and if you eat it, you'll die?" );
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (firstRiddle.equals("nothing")) {
+	 score+=1;
+	 JOptionPane.showMessageDialog(null, "correct");
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "wrong");
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, score);
 	}
 }
 
