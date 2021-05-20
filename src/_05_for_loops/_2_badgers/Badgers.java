f99567b8e512556eb37ba66e12db6b1dced5f7da
haley <haley@DESKTOP-93K8LIV>
2021-05-20 18:57:32 -0500

    i finished badgers my ages and rocket ship

---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,27 @@
+package _05_for_loops._2_badgers;
+
+public class Badgers {
+public static void main(String[] args) {
+	 
+	for (int age=0; age<= 2; age+=1){
+	for (int e=0; e<= 12; e+=1){
+		System.out.println("badger");
+
+	}
+	for (int i=0; i<= 2; i+=1){
+		System.out.println("mushroom");
+
+	}
+	}
+	System.out.println("snake!!");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
